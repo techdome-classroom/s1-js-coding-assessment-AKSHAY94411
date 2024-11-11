d5bf6d556ede13ed7c9753e72da2d62e3dac822a
AKSHAY94411 <136363668+AKSHAY94411@users.noreply.github.com>
2024-11-11 14:28:06 +0530

    Update program2.js
    code for the count of islands in java script

--- program2.js
+++ program2.js
@@ -1,7 +1,33 @@
 const decodeTheRing = function (s, p) {
 
-    // write your code here
+    // write your code hereclass Solution:
+    def getTotalIsles(self, grid: list[list[str]]) -> int:
+        # Helper function to perform DFS and mark visited land cells
+        def dfs(i, j):
+            # Check if the cell is out of bounds or water
+            if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] == 'W':
+                return
+            # Mark the cell as visited by setting it to 'W'
+            grid[i][j] = 'W'
+            # Explore the neighboring cells (up, down, left, right)
+            dfs(i + 1, j)
+            dfs(i - 1, j)
+            dfs(i, j + 1)
+            dfs(i, j - 1)
+
+        # Count of islands
+        island_count = 0
+
+        # Traverse each cell in the grid
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                # If the cell is land, it's a new island
+                if grid[i][j] == 'L':
+                    island_count += 1
+                    dfs(i, j)  # Perform DFS to mark all connected land cells
+
+        return island_count
+
 
   };
-  
   module.exports = decodeTheRing;
